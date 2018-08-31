563f4aa3e799fda9616f1769d2d3c846aa22924a
ragu <ragu25@codingtown.com>
2018-08-31 11:48:51 +0530

    Add firebase configuration and signup signin signout navigation screen code changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router } from "react-router-dom";
-// import Navigation from "./Navigation";
-//
-// const App = () => (
-//   <Router>
-//     <Navigation />
-//   </Router>
-// );
-//
-// export default App;
-
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
---
+++ src/firebase/firebase.js
@@ -0,0 +1,30 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const prodConfig = {
+  apiKey: "AIzaSyCNsi07jS7Zwijm_Fh0tcJzQecntiHCbPc",
+  authDomain: "reactfirebase-12818.firebaseapp.com",
+  databaseURL: "https://reactfirebase-12818.firebaseio.com",
+  projectId: "reactfirebase-12818",
+  storageBucket: "reactfirebase-12818.appspot.com",
+  messagingSenderId: "669388632860"
+};
+
+const devConfig = {
+  apiKey: "AIzaSyCNsi07jS7Zwijm_Fh0tcJzQecntiHCbPc",
+  authDomain: "reactfirebase-12818.firebaseapp.com",
+  databaseURL: "https://reactfirebase-12818.firebaseio.com",
+  projectId: "reactfirebase-12818",
+  storageBucket: "reactfirebase-12818.appspot.com",
+  messagingSenderId: "669388632860"
+};
+
+const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export { auth };
