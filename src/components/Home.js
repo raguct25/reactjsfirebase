aa483180e80e1ffff41bcdcbf7a7c2875f52172a
ragu <ragu25@codingtown.com>
2018-09-02 22:57:08 +0530

    Create database

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,14 +8,58 @@
 //
 // export default HomePage;
 
-import React from "react";
+// import React from "react";
+//
+// import withAuthorization from "./withAuthorization";
+//
+// const HomePage = () => (
+//   <div>
+//     <h1>Home Page</h1>
+//     <p>The Home Page is accessible by every signed in user.</p>
+//   </div>
+// );
+//
+// const authCondition = authUser => !!authUser;
+//
+// export default withAuthorization(authCondition)(HomePage);
+
+import React, { Component } from "react";
 
 import withAuthorization from "./withAuthorization";
+import { db } from "../firebase";
 
-const HomePage = () => (
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: null
+    };
+  }
+
+  componentDidMount() {
+    db
+      .onceGetUsers()
+      .then(snapshot => this.setState({ users: snapshot.val() }));
+  }
+
+  render() {
+    const { users } = this.state;
+    return (
+      <div>
+        <h1>Home</h1>
+        <p>The Home Page is accessible by every signed in user.</p>
+        {!!users && <UserList users={users} />}
+      </div>
+    );
+  }
+}
+const UserList = ({ users }) => (
   <div>
-    <h1>Home Page</h1>
-    <p>The Home Page is accessible by every signed in user.</p>
+    <h2>List of Usernames of Users</h2>
+    <p>(Saved on Sign Up in Firebase Database)</p>
+
+    {Object.keys(users).map(key => <div key={key}>{users[key].username}</div>)}
   </div>
 );
 
